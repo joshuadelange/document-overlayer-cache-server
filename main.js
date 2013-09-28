d6780acc2e4fe70981bb226d5a422cd26319902a
Joshua de Lange <joshua.delange@gmail.com>
2013-09-29 00:54:23 +0200

    makes stuff more solid!

--- main.js
+++ main.js
@@ -1,139 +1,235 @@
-'use strict' ;
+var http = require('http'),
+    https = require('https'),
+    fs = require('fs') ;
 
-$(document).ready(function(){
+var Server = {
 
-  var http = require('http'),
-      https = require('https'),
-      fs = require('fs'),
-      userHomeDir = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'],
-      saveDir = userHomeDir + '/Documents/document-overlayer-cache/' ;
+  saveDir: process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'] + '/Documents/document-overlayer-cache/',
 
-  console.log('checking if ', saveDir, 'exists...') ;
-  fs.mkdir(saveDir, function(){
-    console.log('created save dir', arguments) ;
-    $('.saveLocation').html('Saving files in: ' + saveDir) ;
-  }) ;
+  url: '',
+  documentName: '',
+  docSaveDir: '',
 
-  http.createServer(function (req, res) {
+  baseImageURL: '',
+  numberOfPages: '',
 
-    var urlParts = req.url.split('/') ;
+  pageNumber: null,
+  pageSaveLocation: '',
 
-    console.log(urlParts) ;
+  // console.log('checking if ', saveDir, 'exists...') ;
+  // fs.mkdir(saveDir, function(){
+  //   console.log('created save dir', arguments) ;
+  //   $('.saveLocation').html('Saving files in: ' + saveDir) ;
+  // }) ;
 
-    if(urlParts[1].indexOf('http') > -1) {
+  init: function(){
 
-      var url = decodeURIComponent(urlParts[1]) ;
+    var given = this ;
 
-      $('.log').prepend('<li>Waiting for Google\'s magic...</li>') ;
+    http.createServer(function (req, res) {
 
-      $('iframe').remove() ; //double check
-      var $iframe = $('<iframe />').attr('src', url) ;
-      $('body').append($iframe) ;
+      var urlParts = req.url.split('/') ;
+      console.log('url parts', urlParts) ;
 
-      $('iframe').load(function(){
-  
-        console.log('iframe loaded') ;
+      //first parameter = url (of google doc viewer)
+      if(urlParts[1].indexOf('http') > -1) {
 
-        var baseImageURL = 'https://docs.google.com/viewer' + $('iframe').contents().find('.page-image:nth-of-type(2)').attr('src'),
-            numberOfPages = parseInt($('iframe').contents().find('#controlbarPageNumber').html().split(' / ')[1], 10),
-            documentName = decodeURIComponent(decodeURIComponent(baseImageURL).replace('https://docs.google.com/viewer?url=https://dl.dropboxusercontent.com/s/', '').split('/')[1].split('?')[0]),
-            docSaveDir = saveDir + documentName.replace(' ', '-').replace('.', '-') ;
+        given.url = decodeURIComponent(urlParts[1]) ;
+        given.loadDocument(function(){
 
-        console.log(numberOfPages) ;
-        console.log(baseImageURL) ;
-        console.log(documentName) ;
-        console.log(docSaveDir) ;
+          if(urlParts[2] && parseInt(urlParts[2], 10) > 0) {
 
-        $('.log').prepend('<li>Downloading ' + numberOfPages + ' pages for ' + documentName + '</li>') ;
+            console.log('got an page number!') ;
 
-        fs.mkdir(docSaveDir, function(){
+            given.pageNumber = parseInt(urlParts[2], 10) ;
 
-          console.log('doc folder is ready') ;
+            given.downloadPage(given.pageNumber, function(){
 
-          var downloadNextPage = function(i){
+              console.log('got page') ;
 
-            console.log('attemtping to save page ', i) ;
+              //output png here
+              var img = fs.readFileSync(given.pageSaveLocation);
+              res.writeHead(200, {'Content-Type': 'image/png' });
+              res.end(img, 'binary');
 
-            var pageImageURL = baseImageURL.replace('pagenumber=1', 'pagenumber=' + i),
-                pageSaveLocation = docSaveDir + '/page-' + i + '.png' ;
-    
-            fs.exists(pageSaveLocation, function(exists) {
+            }) ;
 
-              console.log('file exists:', exists) ;
-
-              if(!exists) {
-
-                console.log('starting file download request') ;
-
-                https.get(pageImageURL, function(response) {
-
-                  var imageData = '' ;
-
-                  response.setEncoding('binary') ;
-
-                  response.on('data', function(chunk) {
-                    console.log('writing data') ;
-                    imageData += chunk;
-                  });
-
-                  response.on('end', function () {
-
-                    console.log('end of receiving') ;
-            
-                    fs.writeFile(pageSaveLocation, imageData, 'binary', function(err){
-
-                      if(err) {
-                        throw err;
-                      }
-
-                      console.log('File: ' + pageSaveLocation + ' written!');
-
-                    }) ;
-
-                    $('.log li:first').html('<li>Downloading ' + i + '/' + numberOfPages + ' pages for ' + documentName + '</li>') ;
-
-                    if(i > 1) {
-                      downloadNextPage(i - 1) ;
-                    }
-
-                    if(i === 0) {
-                      $('.log').prepend('<li>Finished downloading ' + documentName + '</li>') ;
-                      $('iframe').remove() ;
-                    }
-
-                  });
-
-                });
-
-              }
-              else {
-                console.log('file already exists') ;
-
-                if(i > 1) {
-                  downloadNextPage(i - 1) ;
-                }
-
-              }
-
-            });
-
-          } ;
-
-          downloadNextPage(numberOfPages) ;
+          }
+          else {
+            //cache everything!
+            given.downloadNextPage(given.numberOfPages) ;
+          }
 
         }) ;
 
-      }) ;
+      }
+      else {
+  
+        //for the sake of pinging
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('success');
+        
+      }
 
-    }
 
-    console.log(req, res) ;
+    }).listen(1337, '127.0.0.1');
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('success');
+    $('.status').html('Ready for caching') ;
 
-  }).listen(1337, '127.0.0.1');
+  }, //init
 
-  $('.status').html('Ready for caching') ;
+  loadDocument: function(cb){
 
-}) ;
+    var given = this,
+        stuffWeDontCareAbout = 'https://docs.google.com/viewer?url=https://dl.dropboxusercontent.com/s/' ;
+    given.documentName = decodeURIComponent(decodeURIComponent(given.url).replace(stuffWeDontCareAbout, '').split('/')[1].split('?')[0]),
+
+    given.docSaveDir = given.saveDir + given.documentName.replace(' ', '-').replace('.', '-') ;
+
+    //check if we have a folder
+    fs.exists(given.docSaveDir, function(exists) {
+
+      console.log('doc folder exists', exists) ;
+
+      if(exists) {
+
+        //read the info!
+        fs.readFile(given.docSaveDir + '/_info.txt', function(err, f){
+
+          var params = JSON.parse(f.toString()) ;
+
+          console.log('params', params) ;
+
+          given.baseImageURL = params.baseImageURL ;
+          given.numberOfPages = params.numberOfPages ;
+
+          $('.log').prepend('<li>Found ' + given.documentName + ' in cache') ;
+
+          //fire callback!
+          cb() ;
+
+        });
+
+      }
+      else {
+
+        $('.log').prepend('<li>Waiting for Google\'s magic...</li>') ;
+
+        $('iframe').remove() ; //double check
+        var $iframe = $('<iframe />').attr('src', given.url) ;
+        $('body').append($iframe) ;
+
+        $('iframe').load(function(){
+
+          fs.mkdir(given.docSaveDir, function(){
+
+            console.log('iframe loaded') ;
+
+            given.baseImageURL = 'https://docs.google.com/viewer' + $('iframe').contents().find('.page-image:nth-of-type(2)').attr('src') ;
+            given.numberOfPages = parseInt($('iframe').contents().find('#controlbarPageNumber').html().split(' / ')[1], 10) ;
+
+            fs.writeFileSync(given.docSaveDir + '/_info.txt', JSON.stringify(given), 'UTF-8', {'flags': 'w+'});
+
+            $('.log').prepend('<li>Downloading ' + given.numberOfPages + ' pages for ' + given.documentName + '</li>') ;
+
+            console.log('doc folder is ready') ;
+
+            cb(); //ready for
+
+          }) ;
+
+        }) ;
+
+      }
+    
+    }) ;
+
+  }, //load document
+
+  downloadNextPage: function(i){
+
+    var given = this ;
+    given.downloadPage(i, function(){
+  
+      $('.log li:first').html('<li>Downloading ' + i + '/' + given.numberOfPages + ' pages for ' + given.documentName + '</li>') ;
+
+      if(i > 1) {
+        given.downloadNextPage(i - 1) ;
+      }
+  
+      if(i === 1) {
+        $('.log').prepend('<li>Finished downloading ' + given.documentName + '</li>') ;
+        $('iframe').remove() ;
+      }
+
+    }) ;
+
+  },
+
+  downloadPage: function(pageNumber, cb){
+
+    console.log('attemtping to save page ', pageNumber) ;
+
+    var given = this,
+        pageImageURL = given.baseImageURL.replace('pagenumber=1', 'pagenumber=' + pageNumber) ;
+    
+    given.pageSaveLocation = given.docSaveDir + '/page-' + pageNumber + '.png' ;
+
+    fs.exists(given.pageSaveLocation, function(exists) {
+
+      console.log('file exists:', exists) ;
+
+      if(exists) {
+        //we done! cb!
+        cb() ;
+      }
+      else{
+
+        console.log('starting file download request') ;
+
+        var startTime = new Date().getTime() ;
+
+        console.log('downloading ', pageImageURL) ;
+
+        https.get(pageImageURL, function(response) {
+
+          var imageData = '' ;
+
+          response.setEncoding('binary') ;
+
+          response.on('data', function(chunk) {
+            console.log('writing data') ;
+            imageData += chunk;
+          });
+
+          response.on('end', function () {
+
+            console.log('end of receiving') ;
+    
+            fs.writeFile(given.pageSaveLocation, imageData, 'binary', function(err){
+
+              if(err) {
+                throw err;
+              }
+
+              console.log('TOTAL TIME: ', (new Date().getTime() - startTime) / 1000) ;
+
+              console.log('File: ' + given.pageSaveLocation + ' written!');
+
+              cb() ; //callback!
+
+            }) ;
+
+          });
+
+        });
+
+      }
+
+    });
+
+  }, //downloadPage
+
+} ;
+
+Server.init() ;
